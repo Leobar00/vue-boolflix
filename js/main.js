897f517e430b036a12492e8266e684bbb025f7b7
Leonardo <barbierileon@gmail.com>
2020-12-02 14:18:38 +0100

    schede complete

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 const app = new Vue({
     el:'#root',
     data:{
+       search:'',
+       resultArray:[], 
+    },
+    mounted(){
+       
         
+    },
+    computed:{
+        filter(){
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=518b31ac0cdefdc1e2ca95a2535a9e17&query=' + this.search)
+                .then(r => {
+                    r.data.results.forEach(el => {
+                        this.resultArray.push(el)
+
+                    })
+
+
+                });
+            this.search='';
+
+        }
     }
 })
