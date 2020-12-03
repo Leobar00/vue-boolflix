26bc5ec3a46cf2d9eed462aae8eb8d104e1715d8
Leonardo <barbierileon@gmail.com>
2020-12-03 14:55:14 +0100

    milestone 2 e 3

--- js/main.js
+++ js/main.js
@@ -1,24 +1,59 @@
 const app = new Vue({
     el:'#root',
     data:{
-       search:'',
-       resultArray:[], 
+        allElementCall:[], 
+        search:'',      //barra di ricerca
+        resultArray:[],    // array con film
+        serieTv:[],        //arrau con serie tv
+        apiKey:'518b31ac0cdefdc1e2ca95a2535a9e17', //chiave api
+        pageSelect:1,     //selezione per la pagina 
     },
     methods:{
-        filter(){
-            this.resultArray = [];
-            axios.get('https://api.themoviedb.org/3/search/movie?api_key=518b31ac0cdefdc1e2ca95a2535a9e17&query=' + this.search)
-                .then(r => {
-                    r.data.results.forEach(el => {
-                        this.resultArray.push(el)
+        filter(){           //filtrare i risultati in base alla input
+            this.resultArray= []
+            // prima chiamata per film
+            axios.get('https://api.themoviedb.org/3/search/movie',{
+                params:{
+                    'api_key':this.apiKey,
+                    'query':this.search,
 
-                    })
+                }
+            })
+            .then(r => {
+                this.allElementCall= r.data.total_pages;
+                r.data.results.forEach(el => {
+                    this.resultArray.push(el);
+                })
 
+            });
+            this.serieTv = []
+            // seconda chiamata
+            axios.get('https://api.themoviedb.org/3/search/tv',{
+                params:{
+                    'api_key':this.apiKey,
+                    'query':this.search
+                }
+            })
+            .then(s =>{
+                s.data.results.forEach(movie =>{
+                    this.serieTv.push(movie)
+                })
 
-                });
+            })
+            
             
             this.search='';
 
-        }
+        },
+        // valutazioni
+        starsEmpty(n){
+            return Math.ceil(n/2);
+        },
+        
+        // handleImgNotFound(e){
+        //     e.target.src='img'
+        // }
+        
+        
     }
 })
